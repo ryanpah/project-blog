402b07501c0d1d1d05a6300c110b1fab7c6b0b82
Ryan Page <rpage326@gmail.com>
2013-11-24 16:02:21 -0500

    created new rails app

---
+++ project-blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectBlog::Application.config.secret_key_base = 'b9f9123a9af4514f0276825ad95bced241773e89bec1ef608fc886dabd137c1df5abb616d1c6116e42b6c50d69e0ec6a7ed691b76d99f327d45c275d9326d6a7'
---
+++ project-blog/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProjectBlog::Application.config.session_store :cookie_store, key: '_project-blog_session'
