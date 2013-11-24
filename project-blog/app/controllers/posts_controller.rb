20fa746b8021d60ae9a2336e13ee0d73d0649f10
Ryan Page <rpage326@gmail.com>
2013-11-24 17:58:10 -0500

    finished posts/new now doing posts/create

--- project-blog/app/controllers/posts_controller.rb
+++ project-blog/app/controllers/posts_controller.rb
@@ -2,4 +2,12 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		render text: params[:post].inspect
+	end
 end
